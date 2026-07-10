1794327692fa5dc7ce7100f87a9fb262b0cbd628
Rithanya CS <rithanyacs07@gmail.com>
2026-07-10 08:48:28 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,18 @@
+// Last updated: 7/10/2026, 8:48:28 AM
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x<0){
+            return false;
+        }
+
+        int original = x;
+        int reversed = 0;
+
+        while(x != 0){
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+        return original == reversed;
+    }
+}
