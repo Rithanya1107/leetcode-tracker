4af62c770cc8d05ffe9796dcb12f78bfdbcbc43f
Rithanya CS <rithanyacs07@gmail.com>
2026-07-10 08:46:49 +0530

    Create 2248-MinimumCostOfBuyingCandiesWithDiscount

---
+++ 2248-MinimumCostOfBuyingCandiesWithDiscount/2248-MinimumCostOfBuyingCandiesWithDiscount.java
@@ -0,0 +1,18 @@
+// Last updated: 7/10/2026, 8:46:49 AM
+import java.util.Arrays;
+
+class Solution{
+    public int minimumCost(int[] cost){
+        Arrays.sort(cost);
+        int total = 0;
+        int n = cost.length;
+
+        for (int i = n - 1;i >= 0; i -= 3){
+            total += cost[i];
+            if(i - 1 >= 0){
+                total += cost[i - 1];
+            }
+        }
+        return total;
+    }
+}
