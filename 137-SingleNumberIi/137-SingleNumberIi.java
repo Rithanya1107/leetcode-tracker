0b69297ece40228057df8346ff88c207bba50a1d
Rithanya CS <rithanyacs07@gmail.com>
2026-07-10 08:47:59 +0530

    Create 137-SingleNumberIi

---
+++ 137-SingleNumberIi/137-SingleNumberIi.java
@@ -0,0 +1,18 @@
+// Last updated: 7/10/2026, 8:47:59 AM
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for(int i = 0; i < 32; i++){
+            int count = 0;
+            for(int num : nums){
+                if(((num >> i) & 1) == 1){
+                    count++;
+                }
+            }
+            if(count % 3 != 0){
+                result |= (1 << i);
+            }
+        }
+        return result;
+    }
+}
