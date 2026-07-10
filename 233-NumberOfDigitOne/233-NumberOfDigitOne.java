4a05c51c11f3dd606a02d3bbcaa04625fc2ada24
Rithanya CS <rithanyacs07@gmail.com>
2026-07-10 08:47:28 +0530

    Create 233-NumberOfDigitOne

---
+++ 233-NumberOfDigitOne/233-NumberOfDigitOne.java
@@ -0,0 +1,24 @@
+// Last updated: 7/10/2026, 8:47:27 AM
+class Solution {
+    public int countDigitOne(int n) {
+        long factor = 1;
+        int count = 0;
+        while(factor <= n){
+            long lower = n % factor;
+            long current = (n / factor) % 10;
+            long higher = n / (factor * 10);
+
+            if(current == 0){
+                count += higher * factor;
+            }
+            else if (current == 1){
+                count += higher * factor + lower + 1;
+            }
+            else {
+                count += (higher + 1 ) * factor;
+            }
+            factor *= 10;
+        }
+        return count;
+    }
+}
