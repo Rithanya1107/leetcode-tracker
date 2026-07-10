f835e3250fca4523debcb97c5647ee25a9e8b942
Rithanya CS <rithanyacs07@gmail.com>
2026-07-10 08:47:32 +0530

    Create 191-NumberOf1Bits

---
+++ 191-NumberOf1Bits/191-NumberOf1Bits.java
@@ -0,0 +1,10 @@
+// Last updated: 7/10/2026, 8:47:32 AM
+class Solution {
+    public int hammingWeight(int n) {
+       int count = 0;while (n > 0) {
+        count += (n & 1);
+        n >>= 1;
+       }
+       return count;
+    }
+}
