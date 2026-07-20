4d4d523f2cc49ed6ecdb6ef750801c781e32fad5
Rithanya CS <rithanyacs07@gmail.com>
2026-07-20 10:23:24 +0530

    Create 30-SubstringWithConcatenationOfAllWords

---
+++ 30-SubstringWithConcatenationOfAllWords/30-SubstringWithConcatenationOfAllWords.java
@@ -0,0 +1,59 @@
+// Last updated: 7/20/2026, 10:23:21 AM
+import java.util.*;
+
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> res = new ArrayList<>();
+
+        if (s == null || s.length() == 0 || words.length == 0)
+            return res;
+
+        int wordLen = words[0].length();
+        int wordCount = words.length;
+        int totalLen = wordLen * wordCount;
+
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        for (int i = 0; i < wordLen; i++) {
+            int left = i;
+            int count = 0;
+            Map<String, Integer> window = new HashMap<>();
+
+            for (int right = i; right + wordLen <= s.length(); right += wordLen) {
+
+                String word = s.substring(right, right + wordLen);
+
+                if (map.containsKey(word)) {
+                    window.put(word, window.getOrDefault(word, 0) + 1);
+                    count++;
+
+                    while (window.get(word) > map.get(word)) {
+                        String leftWord = s.substring(left, left + wordLen);
+                        window.put(leftWord, window.get(leftWord) - 1);
+                        left += wordLen;
+                        count--;
+                    }
+
+                    if (count == wordCount) {
+                        res.add(left);
+
+                        String leftWord = s.substring(left, left + wordLen);
+                        window.put(leftWord, window.get(leftWord) - 1);
+                        left += wordLen;
+                        count--;
+                    }
+
+                } else {
+                    window.clear();
+                    count = 0;
+                    left = right + wordLen;
+                }
+            }
+        }
+
+        return res;
+    }
+}
