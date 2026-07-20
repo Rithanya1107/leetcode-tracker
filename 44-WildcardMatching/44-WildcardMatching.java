f77644712bc3a3431da6c755894592d009762575
Rithanya CS <rithanyacs07@gmail.com>
2026-07-20 10:22:52 +0530

    Create 44-WildcardMatching

---
+++ 44-WildcardMatching/44-WildcardMatching.java
@@ -0,0 +1,38 @@
+// Last updated: 7/20/2026, 10:22:50 AM
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int i = 0, j = 0;
+        int star = -1, match = 0;
+
+        while (i < s.length()) {
+            // Characters match or '?'
+            if (j < p.length() && (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i))) {
+                i++;
+                j++;
+            }
+            // Found '*'
+            else if (j < p.length() && p.charAt(j) == '*') {
+                star = j;
+                match = i;
+                j++;
+            }
+            // Backtrack to last '*'
+            else if (star != -1) {
+                j = star + 1;
+                match++;
+                i = match;
+            }
+            // No match
+            else {
+                return false;
+            }
+        }
+
+        // Skip remaining '*'
+        while (j < p.length() && p.charAt(j) == '*') {
+            j++;
+        }
+
+        return j == p.length();
+    }
+}
