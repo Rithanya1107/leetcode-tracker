4ced0352ad3d5b7592dab1865597785a399bd163
Rithanya CS <rithanyacs07@gmail.com>
2026-07-20 10:23:45 +0530

    Create 17-LetterCombinationsOfAPhoneNumber

---
+++ 17-LetterCombinationsOfAPhoneNumber/17-LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,44 @@
+// Last updated: 7/20/2026, 10:23:44 AM
+import java.util.*;
+
+class Solution {
+
+    private static final String[] map = {
+        "",     // 0
+        "",     // 1
+        "abc",  // 2
+        "def",  // 3
+        "ghi",  // 4
+        "jkl",  // 5
+        "mno",  // 6
+        "pqrs", // 7
+        "tuv",  // 8
+        "wxyz"  // 9
+    };
+
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+
+        if (digits == null || digits.length() == 0) {
+            return result;
+        }
+
+        backtrack(result, new StringBuilder(), digits, 0);
+        return result;
+    }
+
+    private void backtrack(List<String> result, StringBuilder current, String digits, int index) {
+        if (index == digits.length()) {
+            result.add(current.toString());
+            return;
+        }
+
+        String letters = map[digits.charAt(index) - '0'];
+
+        for (char c : letters.toCharArray()) {
+            current.append(c);
+            backtrack(result, current, digits, index + 1);
+            current.deleteCharAt(current.length() - 1);
+        }
+    }
+}
